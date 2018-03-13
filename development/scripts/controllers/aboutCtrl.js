b73ecbd45eac01a0fccb6487377d9f1f6eb4dc15
Harald Lønsethagen <haraldlons@gmail.com>
2018-03-13 09:03:35 +0000

    add

---
+++ development/scripts/controllers/aboutCtrl.js
@@ -0,0 +1,14 @@
+(function(){
+	'use strict'
+	/*Harald Code*/
+angular.module("mainApp")
+.controller("aboutCtrl", ['$scope', function($scope){
+	var vm = this;
+
+	vm.melding = "Hei fra ABOUTCTRL";
+
+}]); /*End homeCtrl*/
+
+
+
+})(); /*End function scope*/
---
+++ development/scripts/controllers/contactCtrl.js
@@ -0,0 +1,14 @@
+(function(){
+	'use strict'
+	/*Harald Code*/
+angular.module("mainApp")
+.controller("contactCtrl", ['$scope', function($scope){
+	var vm = this;
+
+	vm.melding = "Hei fra Contact Ctrl!";
+
+}]); /*End homeCtrl*/
+
+
+
+})(); /*End function scope*/
